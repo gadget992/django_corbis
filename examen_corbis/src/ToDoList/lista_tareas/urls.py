f9b2dde748cfc624b01cf40b1c7c2d4ba8be2159
David <fernandezdavid1992@gmail.com>
2022-02-04 15:39:48 -0300

    registro usuarios, login y logout listos

--- examen_corbis/src/ToDoList/lista_tareas/urls.py
+++ examen_corbis/src/ToDoList/lista_tareas/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('editar_tarea/<int:pk>/', views.editar_tarea, name="editar_tarea"),
     path('eliminar_tarea/<int:pk>/', views.eliminar_tarea, name="eliminar_tarea"),
+    path('registro/', views.registro, name='registro'),
 ]
